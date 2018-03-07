074c40ce33b48c1a9d1232a76d27556bb4980391
German <german.nikolishin@gmail.com>
2018-03-08 00:55:10 +0300

    prepare for deploy in heroku

--- models_pack/my_loging.py
+++ models_pack/my_loging.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 import logging
 import datetime
+import os
 
 logging.basicConfig(format='%(levelname)-8s [%(asctime)s] %(message)s',
                     level=logging.DEBUG, filename='logs/util_logs.log')
@@ -17,3 +18,8 @@ def warning(msg):
 
 def error(msg):
     logging.error(msg)
+
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
